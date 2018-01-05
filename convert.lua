4a2959c3ffaab19012c294af9fb345c06d8a96ef
Ljeanne <Ljeanne@users.noreply.github.com>
2018-01-05 17:19:45 +0100

    Add files via upload

---
+++ convert.lua
@@ -0,0 +1,19 @@
+
+-- The module name and filename are the same
+local convert = {}
+function convert.ftToCm(feet)
+  return feet * 30.48
+end
+return convert
+1
+2
+3
+4
+5
+6
+-- The module name and filename are the same
+local convert = {}
+function convert.ftToCm(feet)
+  return feet * 30.48
+end
+return convert
